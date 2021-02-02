8d38be21103ad528261fdbd1abfc3eeee7b2c52f
Jose Sainz <joedanielsainz@gmail.com>
2021-02-02 15:58:26 -0700

    app file updateapp file updateapp file updat

--- app.js
+++ app.js
@@ -28,36 +28,44 @@ let A2 = document.getElementById('A2')
 let Bb2 = document.getElementById('Bb2')
 let B2 = document.getElementById('B2')
 
+// Note arrays
+
 let notes = [C1, Db1, D1, Eb1, E1, F1, Gb1, G1, Ab1, A1, Bb1, B1, C2, Db2, D2, Eb2, E2, F2, Gb2, G2, Ab2, A2, Bb2, B2]
 
-let whiteNotes = document.querySelectorAll('.white-key')
-let blackNotes = document.querySelectorAll('.black-key')
+let whiteNotes = [C1, D1, E1, F1, G1, A1, B1, C2, D2, E2, F2, G2, A2, B2]
 
-whiteNotes[0].classList.add('set')
-console.log(whiteNotes[0])
+let blackNotes = [Db1, Eb1, Gb1, Ab1, Bb1, Db2, Eb2, Gb2, Ab2, Bb2]
+
+// Set keys to their original color
+
+function whiteBg() {
+  for (i = 0; i < whiteNotes.length; i++){
+    whiteNotes[i].style.background = 'white'
+  }
+}
+
+function blackBg() {
+  for (i = 0; i < blackNotes.length; i++){
+    blackNotes[i].style.background = '#222'
+  }
+}
 
 function selectRoot() {
   let rootInput = rootNote.options[rootNote.selectedIndex].value
-  console.log(rootInput)
 
-  if (rootInput === '1') {
+  if (rootInput === '0') {
+    whiteBg()
+    blackBg()
+  } else {
+
+    let intRootInput = parseInt(rootInput)
+
+    for (i = 0; i < intRootInput; i++) {
+      notes[i].style.background = 'red'
     
-  } else if (rootInput ==='2') {
-    notes[0].style.background = 'white'
-    notes[1].style.background = 'red'
-  } else if (rootInput ==='3') {
-    notes[0].style.background = 'white'
-    notes[1].style.background = '#222'
-    notes[2].style.background = 'red'
-  } else if (rootInput ==='4') {
-    notes[0].style.background = 'white'
-    notes[1].style.background = '#222'
-    notes[2].style.background = 'white'
-    notes[3].style.background = 'red'
-  } else if (rootInput === '5') {
-
+    }
   }
-  return false
+  
 }
 
 selectRoot()
